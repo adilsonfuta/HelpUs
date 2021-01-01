b29b5cf8cdd0c9ad1b7f8b2c49b616444fc78ebd
adilson.futa <adilson.futa@jupiter.co.ao>
2021-01-01 14:54:48 +0000

    adicionando base app

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,25 @@ const { request, response } = require('express');
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+app.get('/users', (request, response) => {
     // return response.send("ola mundo");
+    const p=request.params;
+    console.log(p);
+    return response.json(
+        {
+            texto: 'ola mundo',
+            ano: 2021
+        }
+    )
+});
+
+
+app.post('/users', (request, response) => {
+    // return response.send("ola mundo");
+    const p=request.body;
+    console.log(p);
     return response.json(
         {
             texto: 'ola mundo',
---
+++ backend/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+
+  // configuração de acesso ao Banco
+  // ambient de dev
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  // ambiente de hml
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  // ambient de prod
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
