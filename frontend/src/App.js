664f8842501fa8c22f2afddfc99c28b680940f20
adilson.futa <adilson.futa@jupiter.co.ao>
2021-01-04 18:34:12 +0000

    estado e imutabilidade e propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import React, {useState} from "react";
+import Header from "./Header";
+
 
 function App() {
+  
+  // estado e imutabilidade e propriedades
+
+  let [contador,setContador] = useState(0);
+  function adicionar() {
+    setContador(contador+1);
+    console.log(contador)
+  }
   return (
-     <h1> ola mundo </h1>
+    <div>
+      <Header >Contador: {contador} </Header>
+      <button onClick={adicionar}>Iniciar</button>
+    </div>
+
   );
 }
 
