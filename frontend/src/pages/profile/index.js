7dadd3cf784c073f095717f2905b66edd64e0b47
adilson.futa <adilson.futa@jupiter.co.ao>
2021-01-07 18:04:32 +0000

    finalizando o logout

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/all";
 
 import './style.css';
@@ -11,6 +11,7 @@ export default function Profile() {
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
     const [casos, setcasos] = useState([]);
+    const history=useHistory();
 
     useEffect(() => {
         api.get('profile', {
@@ -23,6 +24,25 @@ export default function Profile() {
             }
         )
     }, [ongId]);
+
+   async function EliminarCasos(id) {
+        try {
+            await api.delete(`incidents/${id}`,{
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+            setcasos(casos.filter(caso => caso.id!==id));
+        } catch (error) {
+                alert('erro ao Deletar caso');
+        }
+    }
+
+    function FazerLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -30,7 +50,7 @@ export default function Profile() {
                 <span> Bem Vinda, {ongName} </span>
                 <Link className="button" to="/casos/new">
                     Cadastrar Novo Caso</Link>
-                <button type="button">
+                <button type="button" onClick={FazerLogout}>
                     <FiPower size={18} color="#e02041" ></FiPower>
                 </button>
             </header>
@@ -46,8 +66,8 @@ export default function Profile() {
                         <strong>DESCRIÇÃO</strong>
                         <p>{caso.descriptions}</p>
                         <strong>VALOR:</strong>
-                        <p> { Intl.NumberFormat('pt',{style:'currency', currency:'AOA'}).format(caso.value)} </p>
-                        <button type="button">
+                        <p> { Intl.NumberFormat('pt',{style:'currency', currency:'aoa'}).format(caso.value)} </p>
+                        <button type="button" onClick={()=>EliminarCasos(caso.id)}>
                             <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
                         </button>
                     </li>
