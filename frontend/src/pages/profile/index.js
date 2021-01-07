2bf41b89fc355e5f6e3d240b8b545fb291738721
adilson.futa <adilson.futa@jupiter.co.ao>
2021-01-07 17:33:25 +0000

    finalizando a estrutura dos frontends

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,72 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiPower, FiTrash2 } from "react-icons/all";
 
-import './style.css'
+import './style.css';
 import logoImg from "../../assets/logo.svg";
+import api from "../../services/API";
 
-export default function Profile(){
-    return(
-       <div className="profile-container">
-           <header>
-               <img src={logoImg} alt="seja um ajudador"/>
-               <span> Bem Vinda, NovaDev </span>
-               <Link className="button" to="/casos/new">
-                   Cadastrar Novo Caso</Link>
-               <button type="button">
-                   <FiPower size={18} color="#e02041" ></FiPower>
-               </button>
-           </header>
+export default function Profile() {
 
-           <h1> Casos Cadastrados</h1>
+    const ongId = localStorage.getItem('ongId');
+    const ongName = localStorage.getItem('ongName');
+    const [casos, setcasos] = useState([]);
 
-           <ul>
-               <li>
-                   <strong>CASO:</strong>
-                   <p>Caso de Teste</p>
-                   <strong>DESCRIÇÃO</strong>
-                   <p>Descrição Teste</p>
-                    <strong>VALOR:</strong>
-                    <p> 400.000 </p>
-                    <button type="button">
-                            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-               </li>      
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(
+            response => {
+                setcasos(response.data)
+            }
+        )
+    }, [ongId]);
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="seja um ajudador" />
+                <span> Bem Vinda, {ongName} </span>
+                <Link className="button" to="/casos/new">
+                    Cadastrar Novo Caso</Link>
+                <button type="button">
+                    <FiPower size={18} color="#e02041" ></FiPower>
+                </button>
+            </header>
 
-                <li>
-                   <strong>CASO:</strong>
-                   <p>Caso de Teste</p>
-                   <strong>DESCRIÇÃO</strong>
-                   <p>Descrição Teste</p>
-                    <strong>VALOR:</strong>
-                    <p> 400.000 </p>
-                    <button type="button">
+            <h1> Casos Cadastrados</h1>
+
+            <ul>
+                {casos.map(caso =>
+                (
+                    <li key={caso.id}>
+                        <strong>CASO:</strong>
+                        <p>{caso.title}</p>
+                        <strong>DESCRIÇÃO</strong>
+                        <p>{caso.descriptions}</p>
+                        <strong>VALOR:</strong>
+                        <p> { Intl.NumberFormat('pt',{style:'currency', currency:'AOA'}).format(caso.value)} </p>
+                        <button type="button">
                             <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-               </li>  
-               <li>
-                   <strong>CASO:</strong>
-                   <p>Caso de Teste</p>
-                   <strong>DESCRIÇÃO</strong>
-                   <p>Descrição Teste</p>
-                    <strong>VALOR:</strong>
-                    <p> 400.000 </p>
-                    <button type="button">
-                            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-               </li>  
-               <li>
-                   <strong>CASO:</strong>
-                   <p>Caso de Teste</p>
-                   <strong>DESCRIÇÃO</strong>
-                   <p>Descrição Teste</p>
-                    <strong>VALOR:</strong>
-                    <p> 400.000 </p>
-                    <button type="button">
-                            <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
-                    </button>
-               </li>          
-           </ul>
-       </div>
+                        </button>
+                    </li>
+
+                ))}
+
+            </ul>
+        </div>
     );
 } 
--- frontend/src/pages/register/index.js
+++ frontend/src/pages/register/index.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from 'react-icons/all';
 
 import logoImg from "../../assets/logo.svg";
+import api from "../../services/API";
 
 
 export default function Register() {
+
+    // aqui fica a funcao da api_register
+    const [name, setname] = useState('');
+    const [email, setemail] = useState('');
+    const [whatsapp, setwhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const history=useHistory();
+
+    async function SaveRegistro(e) {
+        e.preventDefault();
+        const data = { name, email, whatsapp, city, uf };
+        try {
+            const response = await api.post('ongs', data);
+            alert(` ID de Acesso: ${response.data.id}`);
+            history.push('/');
+        } catch (error) {
+                alert('Erro no Cadastro, Tente Novamente');
+        }
+    }
+
+
+
+    // aqui fica os dados do component
     return (
         <div className="register-container">
             <div className="content">
@@ -15,18 +41,24 @@ export default function Register() {
                     <h1>Cadastro</h1>
                     <p> Faça o Cadastro, entre na plataforma e ajude a comunidade a crescer.</p>
                     <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="#E02041" /> 
-                        Ja Tenho Registro 
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Ja Tenho Registro
                     </Link>
                 </section>
-                <form>
-                    <input  placeholder="Nome da ONG" />
-                    <input type="email" placeholder="Email" />
-                    <input  placeholder="WhatsApp" />
+                <form onSubmit={SaveRegistro}>
+                    <input placeholder="Nome da ONG" value={name} onChange={e => setname(e.target.value)} />
+                    <input type="email" placeholder="Email" value={email} onChange={e => setemail(e.target.value)} />
+                    <input placeholder="WhatsApp" value={whatsapp} onChange={e => setwhatsapp(e.target.value)} />
 
                     <div className="input-group">
-                    <input  placeholder="Cidade" />
-                    <input  placeholder="UF"  style={{width:80}} />
+
+                        <input placeholder="Cidade"
+                            value={city}
+                            onChange={e => setCity(e.target.value)} />
+
+                        <input placeholder="UF"
+                            style={{ width: 80 }} value={uf}
+                            onChange={e => setUf(e.target.value)} />
                     </div>
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
